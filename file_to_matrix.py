5b5b0b5aff638a8e089ace6fde683e1ea5afcd2b
massard-t <massar_t@etna-alternance.net>
2016-02-14 13:50:49 +0100

    test numpy loadtxt

--- file_to_matrix.py
+++ file_to_matrix.py
@@ -1,9 +1,21 @@
-import numpy
+#!/usr/bin/python3.5
+import numpy as np
+import sys
+from io import StringIO
 
 
 def txt_to_matrix(ftxt):
-    ftxt.replace("@", "2")
-    ftxt.replace(" ", "0")
-    ftxt.replace("+", "1")
-    ftxt.replace("#", "3")
+    with open(ftxt, 'r') as f:
+        ftxt = f.read()
+    ftxt = ftxt.replace(' ', '0')
+    ftxt = ftxt.replace('+', '1')
+    ftxt = ftxt.replace('@', '2')
+    ftxt = ftxt.replace('#', '3')
     print(ftxt)
+    txt = StringIO(ftxt)
+    matrix = np.loadtxt(txt, delimiter='\n')
+    # print(matrix)
+    return (matrix)
+
+if __name__ == '__main__':
+    txt_to_matrix(sys.argv[1])
---
+++ howto_numpy.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.5
+import numpy as np
+
+
+def numpy_test():
+    np.loadtxt("maps/test1.txt")
